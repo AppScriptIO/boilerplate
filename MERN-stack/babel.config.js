4a905f17234d5266818ad5e353955a6bcc69259f
Safi <nassarsafi@gmail.com>
2020-04-08 16:14:41 +0300

    📄 Update configurations for babel.

--- MERN-stack/babel.config.js
+++ MERN-stack/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     "presets": [
       ["@babel/preset-env", { 
+        // adds polyfills globally, exclusive to @babel/transform-runtime
         // "useBuiltIns": "usage", 
         // "corejs": { 
         //   "version": "3", 
@@ -10,9 +11,10 @@ module.exports = {
       }],
       ["@babel/preset-react", { 
         // "runtime": "automatic" 
-        }]
+      }]
     ],
     "plugins": [
+      ["@babel/plugin-transform-runtime", { corejs: 3 }],
       "react-hot-loader/babel"
     ]
 }
